4e3ca9b53de002545f10237c66ffc79677daf76f
Kristine Gudavadze <kristine.gudavade@gmail.com>
2026-02-06 20:03:20 +0400

    Add Event and Ticket ViewSets with DRF ModelViewSet

--- events/views.py
+++ events/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Event, Ticket
+from .serializers import EventSerializer, TicketSerializer
 
-# Create your views here.
+
+class EvenViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
