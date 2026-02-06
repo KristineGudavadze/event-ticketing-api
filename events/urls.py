07edd26908d9f870e334c10e827f04442cdfa2c5
Kristine Gudavadze <kristine.gudavade@gmail.com>
2026-02-07 02:28:51 +0400

    Fix ViewSet typo and update router registration

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,10 @@
 from rest_framework import routers
-from .views import EvenViewSet, TicketViewSet
+from .views import EventViewSet, TicketViewSet
 from django.urls import path, include
 
 
 router = routers.DefaultRouter()
-router.register(r'events', EvenViewSet)
+router.register(r'events', EventViewSet)
 router.register(r'tickets', TicketViewSet)
 
 urlpatterns = [
